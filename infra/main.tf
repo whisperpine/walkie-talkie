4be69ae9b1aeeb35e5347486f68d3afee01b4c0d
Yusong <yusonglai64@gmail.com>
2025-07-15 18:22:20 +0800

    feat(infra): use sops to encrypt sensitive variables

--- infra/main.tf
+++ infra/main.tf
@@ -5,26 +5,48 @@ resource "random_password" "tunnel_secret" {
   length = 64
 }
 
+# sops_file data docs:
+# https://registry.terraform.io/providers/carlpett/sops/latest/docs/data-sources/file
+data "sops_file" "default" {
+  source_file = "encrypted.tfvars.json"
+}
+
+locals {
+  # Cloudflare API token created at https://dash.cloudflare.com/profile/api-tokens
+  cloudflare_token = data.sops_file.default.data["cloudflare_token"]
+  # Zone is the domain (e.g. example.com)
+  cloudflare_zone = data.sops_file.default.data["cloudflare_zone"]
+  # Zone ID for your domain
+  cloudflare_zone_id = data.sops_file.default.data["cloudflare_zone_id"]
+  # Account ID for your Cloudflare account
+  cloudflare_account_id = data.sops_file.default.data["cloudflare_account_id"]
+  # The name of cloudflare tunnel
+  cloudflare_tunnel_name = data.sops_file.default.data["cloudflare_tunnel_name"]
+  # The dns record prefix for websocket api
+  dns_record_prefix_wtws = data.sops_file.default.data["dns_record_prefix_wtws"]
+  # The dns record prefix for REST api
+  dns_record_prefix_wtapi = data.sops_file.default.data["dns_record_prefix_wtapi"]
+}
+
 # cloudflare_zero_trust_tunnel_cloudflared resource docs:
 # https://registry.terraform.io/providers/cloudflare/cloudflare/latest/docs/resources/zero_trust_tunnel_cloudflared
 resource "cloudflare_zero_trust_tunnel_cloudflared" "default" {
-  account_id = var.cloudflare_account_id
-  name       = var.cloudflare_tunnel_name
-  # secret = base64sha256(random_password.tunnel_secret.result)
+  account_id = local.cloudflare_account_id
+  name       = local.cloudflare_tunnel_name
 }
 
 # cloudflare_zero_trust_tunnel_cloudflared_token data docs:
 # https://registry.terraform.io/providers/cloudflare/cloudflare/latest/docs/data-sources/zero_trust_tunnel_cloudflared
 data "cloudflare_zero_trust_tunnel_cloudflared_token" "default" {
-  account_id = var.cloudflare_account_id
+  account_id = local.cloudflare_account_id
   tunnel_id  = cloudflare_zero_trust_tunnel_cloudflared.default.id
 }
 
 # cloudflare_dns_record resource docs:
 # https://registry.terraform.io/providers/cloudflare/cloudflare/latest/docs/resources/dns_record
 resource "cloudflare_dns_record" "wtws" {
-  zone_id = var.cloudflare_zone_id
-  name    = var.dns_record_prefix_wtws
+  zone_id = local.cloudflare_zone_id
+  name    = local.dns_record_prefix_wtws
   content = "${cloudflare_zero_trust_tunnel_cloudflared.default.id}.cfargotunnel.com"
   comment = "walkie-talkie's websocket api endpoint"
   type    = "CNAME"
@@ -35,8 +57,8 @@ resource "cloudflare_dns_record" "wtws" {
 # cloudflare_dns_record resource docs:
 # https://registry.terraform.io/providers/cloudflare/cloudflare/latest/docs/resources/dns_record
 resource "cloudflare_dns_record" "wtapi" {
-  zone_id = var.cloudflare_zone_id
-  name    = var.dns_record_prefix_wtapi
+  zone_id = local.cloudflare_zone_id
+  name    = local.dns_record_prefix_wtapi
   content = "${cloudflare_zero_trust_tunnel_cloudflared.default.id}.cfargotunnel.com"
   comment = "walkie-talkie's REST api endpoint"
   type    = "CNAME"
@@ -48,18 +70,18 @@ resource "cloudflare_dns_record" "wtapi" {
 # https://registry.terraform.io/providers/cloudflare/cloudflare/latest/docs/resources/zero_trust_tunnel_cloudflared_config
 resource "cloudflare_zero_trust_tunnel_cloudflared_config" "default" {
   tunnel_id  = cloudflare_zero_trust_tunnel_cloudflared.default.id
-  account_id = var.cloudflare_account_id
+  account_id = local.cloudflare_account_id
   config = {
     ingress = [
       {
-        hostname = "${cloudflare_dns_record.wtws.name}.${var.cloudflare_zone}"
+        hostname = "${cloudflare_dns_record.wtws.name}.${local.cloudflare_zone}"
         service  = "ws://wt-websocket:3000"
         origin_request = {
           no_tls_verify = true
         }
       },
       {
-        hostname = "${cloudflare_dns_record.wtapi.name}.${var.cloudflare_zone}"
+        hostname = "${cloudflare_dns_record.wtapi.name}.${local.cloudflare_zone}"
         service  = "http://wt-rest-api:8080"
         origin_request = {
           no_tls_verify = true
--- infra/providers.tf
+++ infra/providers.tf
@@ -1,15 +1,19 @@
 terraform {
   # version constraints docs:
   # https://developer.hashicorp.com/terraform/language/expressions/version-constraints
-  required_version = ">= 1.7.5"
+  required_version = ">= 1.10"
   required_providers {
+    sops = {
+      source  = "carlpett/sops"
+      version = "~> 1.2.1"
+    }
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = "~> 5.3"
+      version = "~> 5.6"
     }
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.6"
+      version = "~> 3.7"
     }
   }
 }
@@ -17,9 +21,13 @@ terraform {
 # cloudflare/cloudflare provider docs:
 # https://registry.terraform.io/providers/cloudflare/cloudflare/latest/docs
 provider "cloudflare" {
-  api_token = var.cloudflare_token
+  api_token = local.cloudflare_token
 }
 
 # hashicorp/random provider docs:
 # https://registry.terraform.io/providers/hashicorp/random/latest/docs
 provider "random" {}
+
+# carlpett/sops provider docs: 
+# https://registry.terraform.io/providers/carlpett/sops/latest/docs
+provider "sops" {}
