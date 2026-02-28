9bcccfe5f6437fe15d5ccb5fecb5b2eb8b347588
Yusong <yusonglai64@gmail.com>
2026-03-01 01:30:11 +0800

    feat(infra): output the DNS records of "published application routes"

--- infra/cloudflare-tunnel/outputs.tf
+++ infra/cloudflare-tunnel/outputs.tf
@@ -2,3 +2,12 @@ output "tunnel_token" {
   value     = data.cloudflare_zero_trust_tunnel_cloudflared_token.default.token
   sensitive = true
 }
+
+output "tunnel_dns_records" {
+  description = "The DNS records of published application routes"
+  value = [
+    cloudflare_dns_record.wtws.name,
+    cloudflare_dns_record.wtapi.name
+  ]
+  sensitive = true
+}
--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,5 +1,15 @@
+# ------------------------- #
+# module: cloudflare_tunnel
+# ------------------------- #
+
 output "tunnel_token" {
   description = "the token used by Cloudflare Tunnel"
   value       = module.cloudflare_tunnel.tunnel_token
+  sensitive   = true
+}
+
+output "tunnel_dns_records" {
+  description = "The DNS records of published application routes"
+  value       = module.cloudflare_tunnel.tunnel_dns_records
   sensitive   = true
 }
